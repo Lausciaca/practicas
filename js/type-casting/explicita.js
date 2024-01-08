8e22f85a6ed534c3ac78980a2373ae726d863760
Laureano <laureanosciacaluga@gmail.com>
2024-01-08 09:44:31 -0300

    type-casting

---
+++ js/type-casting/explicita.js
@@ -0,0 +1,14 @@
+const cadena = '5';
+const numero = 5;
+const valor = true;
+const nulo = null;
+let indefinido;
+
+console.log(String(numero))
+console.log(Number(cadena))
+console.log(Number(valor))
+console.log(Number(nulo))
+console.log(Number(indefinido))
+console.log(Boolean(indefinido))
+console.log(Boolean(nulo))
+console.log(Boolean(numero))
